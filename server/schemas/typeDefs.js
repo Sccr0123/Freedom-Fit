d265f8bb08f131a3fcca0caec2b14d34aca948b0
Zachary Thomas <sccr0123@gmail.com>
2022-06-30 19:33:03 -0400

    Added User To typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,12 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+	type User {
+		_id: ID
+		firstName: String
+		lastName: String
+		email: String
+	}
+`;
 
 module.exports = typeDefs;
