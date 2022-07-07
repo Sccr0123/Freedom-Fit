31e436a291ed0661e1d646d059380cbeebcb58c1
jolson2811 <jolson2811@gmail.com>
2022-07-07 16:00:51 -0600

    added About component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,16 @@ import Nav from "./components/Nav";
 import CourseOverview from "./components/CourseOverview";
 import CourseSingleView from "./components/CourseSingleView";
 import Home from "./pages/Home";
+import About from "./components/About";
 
 function App() {
-	return (
-		<div className="App">
-			<Nav></Nav>
-			<Home></Home>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <Home></Home>
+      <About></About>
+    </div>
+  );
 }
 
 export default App;
