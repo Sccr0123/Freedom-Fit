ec8d3ebdc62a82c303e3aa339a28316389223f3a
jolson2811 <jolson2811@gmail.com>
2022-07-07 15:59:16 -0600

    started About page setup

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
-	return (
-		<header className="d-flex px-1 p-3 justify-content-between align-items-center bg-danger text-light px-3">
-				<h1>Freedom Fit</h1>
-				<span>Login</span>
-		</header>
-	);
+  return (
+    <header className="d-flex px-1 p-3 justify-content-between align-items-center bg-danger text-light px-3">
+      <h1>Freedom Fit</h1>
+      <span>Login</span>
+    </header>
+  );
 }
 
 export default Nav;
