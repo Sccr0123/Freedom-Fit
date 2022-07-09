8b7646bdf43bcf272e2d741e1c5031bf4d747127
Zachary Thomas <sccr0123@gmail.com>
2022-07-09 12:34:00 -0400

    Completed Shopping Cart

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Cart from "../components/Cart";
+
 // import { capitalizeFirstLetter } from "../utils/helpers";
 
 // const Navigation = () => {
@@ -14,16 +16,17 @@ const Header = () => {
 		<header className="bg-danger mb-4 py-2 flex-row align-center">
 			<div className="container flex-row justify-space-between-lg justify-center align-center">
 				<Link to="/">
-					<h1>FreedomFit</h1>
+					<h1>Freedom Fit</h1>
 				</Link>
 
 				<nav>
 					<Link to="/about">About</Link>
 					<Link to="/courses">Courses</Link>
-					<Link to="/cart">Cart</Link>
+					<Cart />
+					{/* <Link to="/cart"></Link> */}
 					<Link to="/login">Login</Link>
 					<Link to="/signup">Signup</Link>
-					{/* <Cart /> */}
+					
 				</nav>
 			</div>
 		</header>
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -7,6 +7,8 @@ const { Provider } = StoreContext;
 const StoreProvider = ({ value = [], ...props }) => {
 	const [state, dispatch] = useProductReducer({
 		products: [],
+		cart: [],
+		cartOpen: false,
 		categories: [],
 		currentCategory: "",
 	});
