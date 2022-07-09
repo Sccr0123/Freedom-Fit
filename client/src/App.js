20561873255ece288b95de8eb7261b2da4669388
Zachary Thomas <sccr0123@gmail.com>
2022-07-09 02:01:04 -0400

    Added globalState for shopping cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "bootstrap";
 import "react-bootstrap";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
+	ApolloProvider,
+	ApolloClient,
+	InMemoryCache,
+	createHttpLink,
 } from "@apollo/client";
 
 import Header from "./components/Header";
+import { StoreProvider } from "./utils/GlobalState";
 
 //Pages
 import CourseOverview from "./components/CourseOverview";
@@ -19,29 +20,34 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+	uri: "/graphql",
 });
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+	link: httpLink,
+	cache: new InMemoryCache(),
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<div className="flex-column justify-flex-start min-100-vh">
+					<StoreProvider>
+						<Header />
+						<div className="container">
+							<Routes>
+								<Route path="/" element={<Home />} />
+								<Route path="/about" element={<About />} />
+								<Route path="/login" element={<Home />} />
+								<Route path="/signup" element={<Home />} />
+							</Routes>
+						</div>
+					</StoreProvider>
+				</div>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
