cb5d78e0e4500310f845bf276534c9aaa2684d62
jolson2811 <96886608+jolson2811@users.noreply.github.com>
2022-07-09 15:43:47 -0600

    added gradient styling to nav + home body

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,10 +5,8 @@ const Home = () => {
   return (
     <div className="home d-flex h-100 p-4 justify-content-center">
       <div className="col-8 p-3 text-center">
-        <h3 className="text-light">FreedomFit</h3>
-        <p className="text-light text-break">
-          adauidaudhaouishduashdiuoashudhasuidhauishduashduiahsdahsduiashduhasudhasuhdiashdiuashduiashduiashdhuaishdsahdiuashdiashdasddddasdaadauidaudhaouishduashdiuoashudhasuidhauishduashduiahsdahsduiashduhasudhasuhdiashdiuashduiashduiashdhuaishdsahdiuashdiashdasddddasdadauidaudhaouishduashdiuoashudhasuidhauishduashduiahsdahsduiashduhasudhasuhdiashdiuashduiashduiashdhuaishdsahdiuashdiashdasddddasdadauidaudhaouishduashdiuoashudhasuidhauishduashduiahsdahsduiashduhasudhasuhdiashdiuashduiashduiashdhuaishdsahdiuashdiashdasddddasdaadauidaudhaouishduashdiuoashudhasuidhauishduashduiahsdahsduiashduhasudhasuhdiashdiuashduiashduiashdhuaishdsahdiuashdiashdasddddasdaadauidaudhaouishduashdiuoashudhasuidhauishduashduiahsdahsduiashduhasudhasuhdiashdiuashduiashduiashdhuaishdsahdiuashdiashdasddddasdaadauidaudhaouishduashdiuoashudhasuidhauishduashduiahsdahsduiashduhasudhasuhdiashdiuashduiashduiashdhuaishdsahdiuashdiashdasddddasdaadauidaudhaouishduashdiuoashudhasuidhauishduashduiahsdahsduiashduhasudhasuhdiashdiuashduiashduiashdhuaishdsahdiuashdiashdasddddasdaadauidaudhaouishduashdiuoashudhasuidhauishduashduiahsdahsduiashduhasudhasuhdiashdiuashduiashduiashdhuaishdsahdiuashdiashdasddddasdaadauidaudhaouishduashdiuoashudhasuidhauishdu
-        </p>
+        <h1 className="mb-4">FreedomFit</h1>
+        <h2>Live your life free.</h2>
       </div>
     </div>
   );
