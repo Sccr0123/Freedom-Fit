be1cc0e9df0fed98cb53dafee503bd785f5a095d
jolson2811 <jolson2811@gmail.com>
2022-07-09 13:37:45 -0600

    updated nav/header & home page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,7 +12,7 @@ import { capitalizeFirstLetter } from "../utils/helpers";
 
 const Header = () => {
   return (
-    <header className="bg-danger mb-4 py-2 flex-row align-center">
+    <header className="mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
           <h1>FreedomFit</h1>
