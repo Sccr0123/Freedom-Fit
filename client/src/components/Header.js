1d92e81378a2327562f6088cc8a1abac8fed76da
jolson2811 <96886608+jolson2811@users.noreply.github.com>
2022-07-09 15:02:01 -0600

    updated header/nav bar styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,24 +12,24 @@ import Cart from "../components/Cart";
 const Header = () => {
   return (
     <header className="mb-4 py-2 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">
+      <div className="d-flex justify-content-between px-4 header-cont">
+        <Link to="/" className="p-3">
           <h1>FreedomFit</h1>
         </Link>
 
         <nav className="d-flex">
-          <div>
+          <div className="p-3">
             <Link to="/about">About</Link>
           </div>
 
-          <div>
+          <div className="p-3">
             <Link to="/courses">Courses</Link>
           </div>
           {/* <Link to="/cart"></Link> */}
-          <div>
+          <div className="p-3">
             <Link to="/login">Login</Link>
           </div>
-          <div>
+          <div className="p-3">
             <Link to="/signup">Signup</Link>
           </div>
           <Cart />
