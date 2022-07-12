411587e747713bd58b8b9664d13a1f27e3696977
Zachary Thomas <sccr0123@gmail.com>
2022-07-12 00:43:30 -0400

    Fixed Issue With Profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,43 +1,52 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import PurchasedCourses from '../components/PurchasedCourses';
+import React from "react";
+import { useParams } from "react-router-dom";
+import PurchasedCourses from "../components/PurchasedCourses";
 
-import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
-import Auth from '../utils/auth';
-import { Link } from 'react-router-dom';
+import { useQuery } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
 
 const Profile = (props) => {
-  const { email: userParam } = useParams();
+	const { _id: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
-    variables: { email: userParam }
-  });
+	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+		variables: { _id: userParam },
+	});
 
-  const user = data?.user || {};
+	const user = data?.me || data?.user || {};
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+	if (loading) {
+		return <div>Loading...</div>;
+	}
 
-  return (
-    <div>
-    {Auth.loggedIn() ? (
-    <div className="my-account">
-    <div className="my-account__body">
-      <div className="account-dashboard">
-        <h2>Hi, {user.email}</h2>
-        <PurchasedCourses/>
-      </div> 
-    </div>
-  </div>
-    ) : (
-      <div><p className="bg-dark text-light p-3">You must be signed to view this page.</p>
-      <Link to={`/login`}>Login here</Link>
-      </div>
-    )}
-  </div>
-  )
-}
+	if (user) {
+		console.log(user);
+	}
+
+	return (
+		<div>
+			{Auth.loggedIn() ? (
+				<div className="my-account">
+					<div className="my-account__body">
+						<div className="account-dashboard">
+							<h2>
+								Hi, {user.firstName} {user.lastName}
+							</h2>
+							<PurchasedCourses orders={user.orders}/>
+						</div>
+					</div>
+				</div>
+			) : (
+				<div>
+					<p className="bg-dark text-light p-3">
+						You must be signed to view this page.
+					</p>
+					<Link to={`/login`}>Login here</Link>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default Profile;
