cb7a8a8a2a3aa17f49ed6d0c3cd72d32fe190857
jolson2811 <96886608+jolson2811@users.noreply.github.com>
2022-07-11 20:32:46 -0600

    added styling to signup page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -5,83 +5,103 @@ import Auth from "../utils/auth";
 import { ADD_USER } from "../utils/mutations";
 
 function Signup(props) {
-	const [formState, setFormState] = useState({ email: "", password: "" });
-	const [addUser] = useMutation(ADD_USER);
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [addUser] = useMutation(ADD_USER);
 
-	const handleFormSubmit = async (event) => {
-		event.preventDefault();
-		const mutationResponse = await addUser({
-			variables: {
-				email: formState.email,
-				password: formState.password,
-				firstName: formState.firstName,
-				lastName: formState.lastName,
-			},
-		});
-		const token = mutationResponse.data.addUser.token;
-		Auth.login(token);
-	};
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName,
+      },
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
 
-	const handleChange = (event) => {
-		const { name, value } = event.target;
-		setFormState({
-			...formState,
-			[name]: value,
-		});
-	};
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-	return (
-		<div className="container my-1">
-			<Link to="/login">← Go to Login</Link>
+  return (
+    <div className="login container d-flex justify-content-center">
+      <div className="row"></div>
+      <div className="col-md-5">
+        <div className="login card card-01">
+          <Link
+            to="/login"
+            type="submit"
+            className="btn btn-default text-uppercase"
+          >
+            ← Go to Login
+          </Link>
+          <div className="card-body">
+            <span className="badge-box">
+              <i className="fa fa-check"></i>
+            </span>
 
-			<h2>Signup</h2>
-			<form onSubmit={handleFormSubmit}>
-				<div className="flex-row space-between my-2">
-					<label htmlFor="firstName">First Name:</label>
-					<input
-						placeholder="First Name"
-						name="firstName"
-						type="firstName"
-						id="firstName"
-						onChange={handleChange}
-					/>
-				</div>
-				<div className="flex-row space-between my-2">
-					<label htmlFor="lastName">Last Name:</label>
-					<input
-						placeholder="Last Name"
-						name="lastName"
-						type="lastName"
-						id="lastName"
-						onChange={handleChange}
-					/>
-				</div>
-				<div className="flex-row space-between my-2">
-					<label htmlFor="email">Email:</label>
-					<input
-						placeholder="Email"
-						name="email"
-						type="email"
-						id="email"
-						onChange={handleChange}
-					/>
-				</div>
-				<div className="flex-row space-between my-2">
-					<label htmlFor="pwd">Password:</label>
-					<input
-						placeholder="******"
-						name="password"
-						type="password"
-						id="pwd"
-						onChange={handleChange}
-					/>
-				</div>
-				<div className="flex-row flex-end">
-					<button type="submit">Submit</button>
-				</div>
-			</form>
-		</div>
-	);
+            <h2 className="card-title">Signup</h2>
+            <form onSubmit={handleFormSubmit}>
+              <div className="flex-row space-between my-2">
+                <label htmlFor="firstName">First Name:</label>
+                <input
+                  className="input"
+                  placeholder="First Name"
+                  name="firstName"
+                  type="firstName"
+                  id="firstName"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex-row space-between my-2">
+                <label htmlFor="lastName">Last Name:</label>
+                <input
+                  className="input"
+                  placeholder="Last Name"
+                  name="lastName"
+                  type="lastName"
+                  id="lastName"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex-row space-between my-2">
+                <label htmlFor="email">Email:</label>
+                <input
+                  className="input"
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                  id="email"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex-row space-between my-2">
+                <label htmlFor="pwd">Password:</label>
+                <input
+                  className="input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  id="pwd"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex-row flex-end">
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
