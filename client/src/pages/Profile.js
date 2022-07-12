0b209173e7004dab06757d9c477e901d7de50c6a
Elexis Blash <elexisblash@gmail.com>
2022-07-11 22:24:11 -0400

    Added authentication to profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,38 +1,44 @@
-import React from "react";
-import PurchasedCourses from "../components/PurchasedCourses";
-import ProfileNav from "../components/ProfileNav";
-import { useQuery } from "@apollo/client";
-import { QUERY_USER } from "../utils/queries";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import PurchasedCourses from '../components/PurchasedCourses';
+import ProfileNav from '../components/ProfileNav';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
-	const { loading, data } = useQuery(QUERY_USER);
-	let user;
+  const { username: userParam } = useParams();
 
-	if (data) {
-		user = data.user;
-	}
+  const { loading, data } = useQuery(QUERY_USER, {
+    variables: { username: userParam }
+  });
 
-	if (loading) {
-		return <div>Loading...</div>;
-	}
+  const user = data?.user || {};
 
-	console.log(user);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-	return (
-		<div className="my-account">
-			{user ? (
-				<>
-					<ProfileNav />
-					<div className="my-account__body">
-						<div className="account-dashboard">
-							<h1>Hi,</h1>
-							<PurchasedCourses />
-						</div>
-					</div>
-				</>
-			) : null}
-		</div>
-	);
-};
+  return (
+    <div>
+    {Auth.loggedIn() ? (
+    <div className="my-account">
+    <ProfileNav/>
+    <div className="my-account__body">
+      <div className="account-dashboard">
+        <h1>Hi, {user.firstName}</h1>
+        <PurchasedCourses/>
+      </div> 
+    </div>
+  </div>
+    ) : (
+      <div><p className="bg-dark text-light p-3">You must be signed to view this page.</p>
+      <Link to={`/login`}>Login here</Link>
+      </div>
+    )}
+  </div>
+  )
+}
 
 export default Profile;
