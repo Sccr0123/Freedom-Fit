80565ed30c385cceb0f67118a8666fc40b525f2f
jolson2811 <96886608+jolson2811@users.noreply.github.com>
2022-07-11 18:56:55 -0600

    added card/basic styling for login fields

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error }] = useMutation(LOGIN);
 
   const handleFormSubmit = async (event) => {
@@ -30,40 +30,52 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+    <div className="container d-flex justify-content-center">
+      <div className="row"></div>
+      <div className="col-md-4">
+        <div className="card card-01">
+          <Link to="/signup">← Go to Signup</Link>
+          <div className="card-body">
+            <span className="badge-box">
+              <i className="fa fa-check"></i>
+            </span>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between">
-          <label htmlFor="email">Email Address:</label>
-          <input
-            placeholder="Email Address"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
+            <h2 className="card-title">Login</h2>
+            <form onSubmit={handleFormSubmit}>
+              <div className="flex-row space-between">
+                <label htmlFor="email">Email Address:</label>
+                <input
+                  placeholder="Email Address"
+                  name="email"
+                  type="email"
+                  id="email"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex-row space-between">
+                <label htmlFor="pwd">Password:</label>
+                <input
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  id="pwd"
+                  onChange={handleChange}
+                />
+              </div>
+              {error ? (
+                <div>
+                  <p className="error-text">
+                    The provided credentials are incorrect
+                  </p>
+                </div>
+              ) : null}
+              <div className="flex-row flex-end">
+                <button type="submit">Login</button>
+              </div>
+            </form>
           </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Login</button>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
