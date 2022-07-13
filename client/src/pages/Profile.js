14e5f8ac161e14efa0cc012e54e2e82bb076a899
jolson2811 <96886608+jolson2811@users.noreply.github.com>
2022-07-12 19:03:35 -0600

    updated profile styling for no courses purchased

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -8,41 +8,39 @@ import Auth from "../utils/auth";
 import { Link } from "react-router-dom";
 
 const Profile = (props) => {
-	const { _id: userParam } = useParams();
+  const { _id: userParam } = useParams();
 
-	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-		variables: { _id: userParam },
-	});
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { _id: userParam },
+  });
 
-	const user = data?.me || data?.user || {};
+  const user = data?.me || data?.user || {};
 
-	if (loading) {
-		return <div>Loading...</div>;
-	}
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-	return (
-		<div>
-			{Auth.loggedIn() ? (
-				<div className="my-account">
-					<div className="my-account__body">
-						<div className="account-dashboard">
-							<h2>
-								Hi, {user.firstName} {user.lastName}
-							</h2>
-							<PurchasedCourses orders={user.orders}/>
-						</div>
-					</div>
-				</div>
-			) : (
-				<div>
-					<p className="bg-dark text-light p-3">
-						You must be signed to view this page.
-					</p>
-					<Link to={`/login`}>Login here</Link>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {Auth.loggedIn() ? (
+        <div className="my-account">
+          <div className="my-account__body">
+            <div className="account-dashboard">
+              <h5>
+                Hi, {user.firstName} {user.lastName}
+              </h5>
+              <PurchasedCourses orders={user.orders} />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p className="profile p-3">You must be signed to view this page.</p>
+          <Link to={`/login`}>Login here</Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
